18eed75b3a607553f7203c3509d7b8fdc167b19b
AntonioOcampo28051971 <antonioocampo2015@gmail.com>
2024-11-09 17:39:48 -0500

    Add files via upload

---
+++ Tareas/src/DiasImpareasMes.java
@@ -0,0 +1,11 @@
+public class DiasImpareasMes {
+    public static void main(String[] args) {
+        for(int i =1; i < 30; i++){
+            if ((i % 2) != 0) {
+                System.out.println("Dia " + i + " es " + "impar");
+            }
+
+        }
+
+    }
+}
