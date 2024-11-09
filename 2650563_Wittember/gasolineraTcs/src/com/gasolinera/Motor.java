65a23a8e80efe6c54b34181e7d692584ad2bc42f
wittember <79621306+wittember@users.noreply.github.com>
2024-11-09 11:06:04 -0500

    Add files via upload

---
+++ 2650563_Wittember/gasolineraTcs/src/com/gasolinera/Motor.java
@@ -0,0 +1,14 @@
+package com.gasolinera;
+
+
+// Wittember Jara Trebejo 2650563
+
+public class Motor {
+    public static void main(String[] args) {
+        // Instancia
+        ComprarGasolina comprarGasolina = new ComprarGasolina();
+        // Llamada a métodos
+        comprarGasolina.detalleCompra();
+        comprarGasolina.imprimirFactura();
+    }
+}
